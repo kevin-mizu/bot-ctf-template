9222f39a16430c4e00eea7638dd4d279e3350cd1
Kévin Mizu <kevin.mizu@protonmail.com>
2025-03-19 12:16:10 +0100

    :sparkles: update the cron to remove /tmp folders older than 1 minutes

--- Dockerfile
+++ Dockerfile
@@ -2,12 +2,12 @@ FROM alpine:3.21.3
 
 WORKDIR /usr/app
 COPY ./src/package.json .
-RUN apk add --update --no-cache nodejs npm socat chromium-chromedriver && \
-	npm install
+RUN apk add --update --no-cache nodejs npm socat chromium-chromedriver su-exec 	&& \
+	npm install																	&& \
+	adduser -D bot
 
 COPY ./entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
 
 COPY ./src/ .
-USER guest
 CMD ["/bin/sh", "/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,12 @@
   while true; do
   	echo "[$(date +'%T')]> Cleaning chromium processes older than 1 minute...";
     ps -o pid,etime,comm | awk '$3 ~ /chrom/ && $1 != 1 && $2 !~ /^0:/ {print $1}' | xargs -r kill -9;
+    
+  	echo "[$(date +'%T')]> Cleaning /tmp folders owned by bot older than 1 minute...";
+    find /tmp -maxdepth 1 -user bot -mmin +1 -print0 | xargs -r -0 su-exec bot rm -rf
+
     sleep 60;
   done
 ) &
 
-socat TCP-LISTEN:55555,fork,reuseaddr EXEC:"node /usr/app/bot.js",stderr;
+exec su-exec bot socat TCP-LISTEN:55555,fork,reuseaddr EXEC:"node /usr/app/bot.js",stderr
