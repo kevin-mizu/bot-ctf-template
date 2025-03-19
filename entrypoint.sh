7ba4a6cdf3b5a08ddb9562b4aea2356ecb96c31d
Kévin Mizu <kevin.mizu@protonmail.com>
2025-03-19 14:06:23 +0100

    :hammer: stop using a bot user, guest works with su-exec

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,11 @@
   	echo "[$(date +'%T')]> Cleaning chromium processes older than 1 minute...";
     ps -o pid,etime,comm | awk '$3 ~ /chrom/ && $1 != 1 && $2 !~ /^0:/ {print $1}' | xargs -r kill -9;
     
-  	echo "[$(date +'%T')]> Cleaning /tmp folders owned by bot older than 1 minute...";
-    find /tmp -maxdepth 1 -user bot -mmin +1 -print0 | xargs -r -0 su-exec bot rm -rf
+  	echo "[$(date +'%T')]> Cleaning /tmp folders owned by guest older than 1 minute...";
+    find /tmp -maxdepth 1 -user guest -mmin +1 -print0 | xargs -r -0 su-exec guest rm -rf
 
     sleep 60;
   done
 ) &
 
-exec su-exec bot socat TCP-LISTEN:55555,fork,reuseaddr EXEC:"node /usr/app/bot.js",stderr
+exec su-exec guest socat TCP-LISTEN:55555,fork,reuseaddr EXEC:"node /usr/app/bot.js",stderr
